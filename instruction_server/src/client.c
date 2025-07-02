e3d0c77f367f315c75bafdf22544019a7c4274c5
antonioc <antoniochappell@protonmail.com>
2025-07-02 16:37:36 -0500

    server responds to requests and client receives responses

---
+++ instruction_server/src/client.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <lcm/lcm.h>
+#include <team_request_t.h>
+#include <team_response_t.h>
+
+void teamResponseHandler(const lcm_recv_buf_t *rbuf, const char * channel, const team_response_t * msg, void * user) {
+    printf("Heard delivery instructions, deliver to team: %d\n", msg->deliverTo);
+}
+
+int main (int argc, char * argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: client {requestingTeamNumber}");
+    }
+
+    lcm_t * lcm = lcm_create(NULL);
+
+    team_request_t my_data = {
+        .requestingTeam = atoi(argv[1])
+    };
+
+    team_response_t_subscribe(lcm, "responses", teamResponseHandler, NULL);
+
+    team_request_t_publish(lcm, "requests", &my_data);
+
+    while(1) {
+        lcm_handle(lcm);
+    }
+
+    lcm_destroy(lcm);
+    
+    return 0;
+}
--- instruction_server/src/server.c
+++ instruction_server/src/server.c
@@ -4,36 +4,24 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-
+#include <team_request_t.h>
+#include <team_response_t.h>
 
 struct node {
     int key;
     struct node * next;
 };
 
-char * readFromFile(char * filename, char * buffer, int bufferSize) {
-    FILE * f = fopen(filename, "r");
-
-    if (f == NULL) {
-        fprintf(stderr, "file not found\n");
-        return "error";
-    }
-
-    char * token;
-
-    fgets(buffer, bufferSize, f);
-
-    fclose(f);
-
-    printf("%s\n", buffer);
-
-    return buffer;
-}
+struct userdata {
+    lcm_t * lcm;
+    struct node * head;
+};
 
 struct node * createRing(char * buffer, int bufferSize) {
     char * token = strtok(buffer, ", ");
     
     struct node * head = malloc(sizeof(struct node));
+    printf("%d\n", atoi(token));
     head->key = atoi(token);
     
     struct node * t = head;
@@ -49,21 +37,78 @@ struct node * createRing(char * buffer, int bufferSize) {
 
     t->next = head;
 
-    free(t);
-
     return head;
 }
 
-int main (int argc, char * argv) {
-    struct node * head = malloc(sizeof(struct node));
+struct node * getNodeByKey(struct node * head, int key) {
+    struct node * t = head;
+    
+    int firstKey = t->key;
+    do {
+        t = t->next;
+        if (t->key == key) {
+            return t;
+        }
+    } while (t->key != firstKey);
 
+    fprintf(stderr, "key not in list, returning NULL\n");
+    return NULL;
+}
+
+char * readFromFile(char * filename, char * buffer, int bufferSize) {
+    FILE * f = fopen(filename, "r");
+
+    if (f == NULL) {
+        fprintf(stderr, "file not found\n");
+        return "error";
+    }
+
+    char * token;
+
+    fgets(buffer, bufferSize, f);
+
+    fclose(f);
+
+    return buffer;
+}
+
+static void teamRequestHandler(const lcm_recv_buf_t *rbuf, const char * channel, const team_request_t * msg, void * user) {
+    printf("Getting delivery instructions for team: %d\n", msg->requestingTeam);
+
+    struct userdata * data = (struct userdata *)user;
+    struct node * head = data->head;
+    lcm_t * lcm = data->lcm;
+
+    struct node * cur = getNodeByKey(head, msg->requestingTeam);
+
+    if (cur == NULL) {
+        fprintf(stderr, "requesting team not found\n");
+        return;
+    }
+    
+    int deliveryTarget = cur->next->key;
+
+    printf("team %d should deliver to team %d\n", msg->requestingTeam, deliveryTarget);
+
+    team_response_t res = {
+        .requestingTeam = msg->requestingTeam,
+        .deliverTo = deliveryTarget
+    };
+
+    printf("sending delivery instructions\n");
+    team_response_t_publish(lcm, "responses", &res);
+
+    return;
+}
+
+int main (int argc, char * argv) {
     char buffer[100];
 
     if (readFromFile("teammap.txt", buffer, sizeof(buffer)/sizeof(char)) == "error") {
         return 1;
     }
 
-    head = createRing(buffer, sizeof(buffer)/sizeof(char));
+    struct node * head = createRing(buffer, sizeof(buffer)/sizeof(char));
 
     lcm_t * lcm = lcm_create(NULL);
 
@@ -72,5 +117,19 @@ int main (int argc, char * argv) {
         return 1;
     }
 
+    // subscriptions
+    struct userdata * data = malloc(sizeof(struct userdata));
+    data->lcm = lcm;
+    data->head = head;
+
+    team_request_t_subscribe(lcm, "requests", teamRequestHandler, (void *)data);
+
+    printf("Listening for requests on channel requests\n");
+
+    while(1) {
+        lcm_handle(lcm);
+    }
+
+    lcm_destroy(lcm);
     return 0;
 }
